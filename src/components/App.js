1a8cf66f0a68d0f04b40db1525d8e63e72b17d60
Priyansh <priyansh.mishra@maximl.com>
2021-02-09 22:59:31 +0530

    task done

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,26 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-		this.activateLasers = this.activateLasers.bind(this);
-	};
+  constructor(props) {
+    super(props);
+    this.activateLasers = this.activateLasers.bind(this);
+  }
 
-	activateLasers(){
-		console.log("click");
-		document.getElementById("para").innerHTML = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-	}
+  activateLasers() {
+    console.log("click");
+    document.getElementById("para").innerHTML =
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  }
 
-    render() {
-    	return(
-    		<div id="main">{ 
-				
-				<button onClick={this.activateLasers}>Click Me</button>
-				}
-				<p id="para"></p>
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        {<p id="para"></p>}
+        {<button onClick={this.activateLasers}>Click Me</button>}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
