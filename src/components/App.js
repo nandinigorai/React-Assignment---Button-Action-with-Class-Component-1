629010bee67b5b73c28ff13b4d0ea272523aa4d6
Priyansh <priyansh.mishra@maximl.com>
2021-02-10 00:17:21 +0530

    task done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-cmport React, { Component, useState } from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 class App extends Component {
