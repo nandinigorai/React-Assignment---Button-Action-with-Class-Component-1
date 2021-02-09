03bff52c62cbd967d5c4ba85f226b11e7ac0e76d
Priyansh <priyansh.mishra@maximl.com>
2021-02-09 22:56:59 +0530

    task done

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,21 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.activateLasers = this.activateLasers.bind(this);
 	};
+
+	activateLasers(){
+		console.log("click");
+		document.getElementById("para").innerHTML = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+	}
 
     render() {
     	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
+    		<div id="main">{ 
+				
+				<button onClick={this.activateLasers}>Click Me</button>
+				}
+				<p id="para"></p>
     		</div>
     	);
     }
